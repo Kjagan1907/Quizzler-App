a76036f9f268d38edb244ed812e10a4e9071f2bc
Kjagan1907 <Kjagan304@gmail.com>
2025-10-07 15:15:24 +0200

    running version

--- ui.py
+++ ui.py
@@ -19,7 +19,8 @@ class QuizInterface():
             text="Some Question Text", 
             fill=THEME_COLOR, 
             font=("Arial", 20, "italic"))
-        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)     
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)   
+        self.get_next_question()  
         true_image = PhotoImage(file="true.png")
         self.true_button = Button(image=true_image, highlightthickness=0, command=self.true_pressed)
         self.true_button.grid(row=2, column=0)
@@ -27,19 +28,34 @@ class QuizInterface():
         self.false_button = Button(image=false_image, highlightthickness=0, command=self.false_pressed)
         self.false_button.grid(row=2, column=1)
 
-        self.get_next_question()
 
         self.window.mainloop()
 
     def get_next_question(self):
+        self.canvas.config(bg="white")
+        if not self.quiz.still_has_questions():
+            self.canvas.itemconfig(self.question_text, text="You've reached the end of the quiz")
+            self.true_button.config(state="disabled")
+            self.false_button.config(state="disabled")
+            return
         q_text = self.quiz.next_question()
         self.canvas.itemconfig(self.question_text, text=q_text)
 
+
     def true_pressed(self):
-        self.quiz.check_answer("True")
+        self.give_feedback(self.quiz.check_answer("True"))
 
     def false_pressed(self):
-        self.quiz.check_answer("False")
+        self.give_feedback(self.quiz.check_answer("False"))
+    
+    def give_feedback(self, check_answer):
+        if check_answer:
+            self.canvas.config(bg="green")
+            self.score_label.config(text=f"Score: {self.quiz.score}")
+        else:
+            self.canvas.config(bg="red")
+            self.score_label.config(text=f"Score: {self.quiz.score}")
+        self.window.after(1000, self.get_next_question)
         
 
 
